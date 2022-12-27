4a69e83b70ddb69c735c1d6ddc4bfb38f299f2fd
Darjiro <98980657+Darjiro@users.noreply.github.com>
2022-12-27 15:31:56 -0300

    for render.com

--- chatserver.py
+++ chatserver.py
@@ -9,26 +9,26 @@ server.bind((addr, port))
 server.listen()
 
 #Main root
-SCREEN_SIZE = (400,400)
-root_win = ctk.CTk()
-main_frame = ctk.CTkFrame(root_win, width=SCREEN_SIZE[0], height=SCREEN_SIZE[1])
-main_frame.pack()
+# SCREEN_SIZE = (400,400)
+# root_win = ctk.CTk()
+# main_frame = ctk.CTkFrame(root_win, width=SCREEN_SIZE[0], height=SCREEN_SIZE[1])
+# main_frame.pack()
 
 #Display all info here
-text_area_log = ctk.CTkTextbox(main_frame, width=380,height=380, state='disable')
-text_area_log.place(x=SCREEN_SIZE[0]//2,y=SCREEN_SIZE[1]//2, anchor=ctk.CENTER)
+# text_area_log = ctk.CTkTextbox(main_frame, width=380,height=380, state='disable')
+# text_area_log.place(x=SCREEN_SIZE[0]//2,y=SCREEN_SIZE[1]//2, anchor=ctk.CENTER)
 
 clients = []
 usernames = []
 
 
-def log_message(msg, text_box, end_line:bool):
-    text_box.configure(state='normal')
-    if end_line:
-        text_box.insert(ctk.END, msg)
-    else:
-        text_box.insert(ctk.END, msg+'\n')
-    text_box.configure(state='disable')
+# def log_message(msg, text_box, end_line:bool):
+#     text_box.configure(state='normal')
+#     if end_line:
+#         text_box.insert(ctk.END, msg)
+#     else:
+#         text_box.insert(ctk.END, msg+'\n')
+#     text_box.configure(state='disable')
 
 def reply_messages(message, _client):
 
@@ -42,19 +42,19 @@ def recv_msg(client, username):
         try:
             msg = client.recv(1024).decode('utf-8')
             reply_messages(msg, client)
-            log_message(msg, text_area_log, True)
-            # print(msg)
+#             log_message(msg, text_area_log, True)
+            print(msg)
 
         except:                
             print(f'User {username} disconected')
-            log_message(f'User {username} disconected', text_area_log, False)
+#             log_message(f'User {username} disconected', text_area_log, False)
             usernames.remove(username)
             clients.remove(client)
             break
 
 def run():
-    # print('Server running')
-    log_message('Server runnig', text_area_log, False)
+    print('Server running')
+#     log_message('Server runnig', text_area_log, False)
     while True:
         client, addr = server.accept()
         clients.append(client)
@@ -63,9 +63,10 @@ def run():
         
         client.send(str(len(clients)).encode('utf-8'))
         
-        # print(f'User {username} connected')
-        log_message(f'User {username} connected', text_area_log, False)
+        print(f'User {username} connected')
+#         log_message(f'User {username} connected', text_area_log, False)
         threading.Thread(target=recv_msg, args=(client,username)).start()
-        
-threading.Thread(target=run).start()
-root_win.mainloop()
+
+run()
+# threading.Thread(target=run).start()
+# root_win.mainloop()
