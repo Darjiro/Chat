7867b93f65431d5936a7504a19959fd2049d3062
Darjiro <andyale01122177069@gmail.com>
2023-01-05 13:15:05 -0300

    Antes del desastre

--- client.py
+++ client.py
@@ -27,63 +27,61 @@ def recive_mesages(text_area):
         text_area.insert(ctk.END, f'{message}')
         text_area.configure(state='disable')
 
-def chat_screen(username, main_win):
+def connect(username, ip, port, label_error, win):
+    try:
+        client.connect((ip, port))
+        client.send(username.encode('utf-8'))
+        
+        chat_screen(username, label_error, win)
+    except:
+        label_error.configure(text=f'Cant connect to {ip}:{port}') 
     
-    server_status = ''
-    # users_amount = '1'
+def chat_screen(username, win):
+    
     _font = ctk.CTkFont('sans-serif', 20)
 
-    if username:
-        try:
-            client.connect((addr, 9999))
-            client.send(username.encode('utf-8'))
-            users_amount = client.recv(1024).decode('utf-8')
-            server_status = 'Server on'
-        except:
-            server_status = 'Server off'
+    root = ctk.CTk()
+    root.resizable(False, False)
+    root.geometry(f'{SCREEN_SIZE[0]}x{SCREEN_SIZE[1]}')
 
-        root = ctk.CTk()
-        root.resizable(False, False)
-        root.geometry(f'{SCREEN_SIZE[0]}x{SCREEN_SIZE[1]}')
+    frame_top = ctk.CTkFrame(root, width=SCREEN_SIZE[0], height=40,bg_color='gray', fg_color='gray', corner_radius=10)
+    frame_center = ctk.CTkFrame(root, width=SCREEN_SIZE[0], height=SCREEN_SIZE[1]-70)
+    frame_bottom = ctk.CTkFrame(root, width=SCREEN_SIZE[0], height=30,bg_color='gray', fg_color='gray')
+    
+    frame_top.place(x=0, y=0)
+    frame_center.place(x=0, y=40)
+    frame_bottom.place(x=0, y=SCREEN_SIZE[1]-30)
+    
+    #Top
+    label_username = ctk.CTkLabel(frame_top, text=f'{username}', text_color='white', font=_font)
+    label_ip = ctk.CTkLabel(frame_top, text=f'', text_color='white', font=_font)
+    # label_users = ctk.CTkLabel(frame_top, text=f'Users: {users_amount}', text_color='white', font=ctk.CTkFont('sans-serif', 15))
+    
+    label_username.place(x=SCREEN_SIZE[0]//2 - 150 , y=20, anchor=ctk.CENTER)
+    label_ip.place(x=SCREEN_SIZE[0]//2 + 30 , y=20, anchor=ctk.CENTER)
+    # label_users.place(x=SCREEN_SIZE[0]//2 + 140 , y=20, anchor=ctk.CENTER)
+    
+    #Center
+    text_area = ctk.CTkTextbox(frame_center,width=SCREEN_SIZE[0], height=SCREEN_SIZE[1]-70, state='disable')
+    text_area.place(x=0)
 
-        frame_top = ctk.CTkFrame(root, width=SCREEN_SIZE[0], height=40,bg_color='gray', fg_color='gray', corner_radius=10)
-        frame_center = ctk.CTkFrame(root, width=SCREEN_SIZE[0], height=SCREEN_SIZE[1]-70)
-        frame_bottom = ctk.CTkFrame(root, width=SCREEN_SIZE[0], height=30,bg_color='gray', fg_color='gray')
+    #Bottom
+    entry_message = ctk.CTkEntry(frame_bottom,placeholder_text='Type here', height=30, width=SCREEN_SIZE[0]-100, border_width=1)
+    btn_send = ctk.CTkButton(frame_bottom,text='Send', height=30, width=99, border_width=1, command=lambda:send_message(username, entry_message, text_area))
+    
+    entry_message.grid(row=0, column=0)
+    btn_send.grid(row=0,column=1)
+    
+    try:
+        thread = threading.Thread(target=recive_mesages, args=(text_area,))
+        thread.daemon = True
+        thread.start()
         
-        frame_top.place(x=0, y=0)
-        frame_center.place(x=0, y=40)
-        frame_bottom.place(x=0, y=SCREEN_SIZE[1]-30)
-        
-        #Top
-        label_username = ctk.CTkLabel(frame_top, text=f'{username}', text_color='white', font=_font)
-        label_ip = ctk.CTkLabel(frame_top, text=f'{server_status}', text_color='white', font=_font)
-        # label_users = ctk.CTkLabel(frame_top, text=f'Users: {users_amount}', text_color='white', font=ctk.CTkFont('sans-serif', 15))
-        
-        label_username.place(x=SCREEN_SIZE[0]//2 - 150 , y=20, anchor=ctk.CENTER)
-        label_ip.place(x=SCREEN_SIZE[0]//2 + 30 , y=20, anchor=ctk.CENTER)
-        # label_users.place(x=SCREEN_SIZE[0]//2 + 140 , y=20, anchor=ctk.CENTER)
-        
-        #Center
-        text_area = ctk.CTkTextbox(frame_center,width=SCREEN_SIZE[0], height=SCREEN_SIZE[1]-70, state='disable')
-        text_area.place(x=0)
-
-        #Bottom
-        entry_message = ctk.CTkEntry(frame_bottom,placeholder_text='Type here', height=30, width=SCREEN_SIZE[0]-100, border_width=1)
-        btn_send = ctk.CTkButton(frame_bottom,text='Send', height=30, width=99, border_width=1, command=lambda:send_message(username, entry_message, text_area))
-        
-        entry_message.grid(row=0, column=0)
-        btn_send.grid(row=0,column=1)
-        
-        try:
-            thread = threading.Thread(target=recive_mesages, args=(text_area,))
-            thread.daemon = True
-            thread.start()
-            
-        except Exception as e:
-            print(e)   
-                 
-        main_win.destroy()
-        root.mainloop()
+    except Exception as e:
+        print(e)   
+                
+    win.destroy()
+    root.mainloop()
 
 def main_screen():
     root = ctk.CTk()
@@ -92,16 +90,94 @@ def main_screen():
 
     # image_logo = ctk.CTkImage()
     entry_username = ctk.CTkEntry(frame, placeholder_text='Username')
-    # entry_ip = ctk.CTkEntry(frame, placeholder_text='Ip')
-    btn_login = ctk.CTkButton(frame, text='Login', width=70, command=lambda:chat_screen(entry_username.get(), root))
-
-    entry_username.place(x=SCREEN_SIZE[0]//2, y=SCREEN_SIZE[1]//2 + 20, anchor=ctk.CENTER)
-    # entry_ip.place(x=SCREEN_SIZE[0]//2, y=SCREEN_SIZE[1]//2 + 60, anchor=ctk.CENTER)
-    btn_login.place(x=SCREEN_SIZE[0]//2, y=SCREEN_SIZE[1]//2 + 100, anchor=ctk.CENTER)
+    entry_ip = ctk.CTkEntry(frame, placeholder_text='Ip:127.0.0.1')
+    entry_port = ctk.CTkEntry(frame, placeholder_text='Port:9999')
+    btn_login = ctk.CTkButton(frame, text='Login', width=70, command=lambda:login(
+        entry_username,
+        entry_ip,
+        entry_port,
+        label_error,
+        root
+    ))
+    label_error = ctk.CTkLabel(frame, text='', text_color='red')
+    
+    entry_username.place(x=SCREEN_SIZE[0]//2, y=SCREEN_SIZE[1]//2 -30, anchor=ctk.CENTER)
+    entry_ip.place(x=SCREEN_SIZE[0]//2, y=SCREEN_SIZE[1]//2 + 20, anchor=ctk.CENTER)
+    entry_port.place(x=SCREEN_SIZE[0]//2, y=SCREEN_SIZE[1]//2 + 50, anchor=ctk.CENTER)
+    btn_login.place(x=SCREEN_SIZE[0]//2, y=SCREEN_SIZE[1]//2 + 140, anchor=ctk.CENTER)
+    label_error.place(x=SCREEN_SIZE[0]//2, y=SCREEN_SIZE[1]//2 + 100, anchor=ctk.CENTER)
     
     root.mainloop()
     
+def login(entry_username, entry_ip, entry_port, label_error, win):
 
+    username = entry_username.get()
 
+    #ip
+    ip_str = entry_ip.get()
+    ip_int = ''
+    ip_char_point = 0
+    
+    #port
+    port_str = entry_port.get()
+    port_int = None
+    
+    for char in ip_str:
+        if char=='.':
+            ip_char_point+=1
+        else:
+            ip_int += char
+    
+    if username:
+        #Valid ip
+        if ip_str == '':
+            ip_str = '127.0.0.1'
+            
+            if len(port_str) == 4:
+                try:
+                    port_int = int(port_str)
+                    
+                    #If all is ok connect to server
+                    connect(username, ip_str, port_int, label_error, win)           
+                except:
+                    label_error.configure(text='Port no valid')
+            
+            elif port_str == '':
+                port_int = 9999
+                connect(username, ip_str, port_int, label_error, win)
+            
+            else:
+                label_error.configure(text='Port no valid') 
+            
+        #If ip have any text
+        else: 
+            #verificando la longitud correcta
+            if len(ip_str) >= 7 and len(ip_str) <= 15 and ip_char_point == 3:
+                #Verificando q sean numeros
+                try:
+                    ip_int = int(ip_int)             
+                    
+                    #Comprobando el puerto
+                    if len(port_str) == 4:
+                        try:
+                            port_int = int(port_str)
+                            
+                            #If all is ok connect to server
+                            connect(username, ip_str, port_int, label_error, win)
+                        except:
+                            label_error.configure(text='Port no valid')
+                    elif port_str == '':
+                        #If all is ok connect to server
+                        port_int = 9999
+                        connect(username, ip_str, port_int, label_error, win)
+                    else:
+                        label_error.configure(text='Port no valid') 
+                except:
+                    label_error.configure(text='Ip no valid')
+            else:
+                label_error.configure(text='Ip no valid')
+    else:
+        label_error.configure(text='Username required!')
+    
 if __name__=='__main__':
     main_screen()
